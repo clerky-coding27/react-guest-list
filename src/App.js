de94b11d89197faef691d33a457236b2e6f988f1
Clara Mrkos <mrkos.clara@gmail.com>
2023-09-20 16:19:07 +0200

    Set First Name

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import './App.css';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [firstNameInput, setFirstNameInput] = useState('xyz');
+
   return (
     <div className="App">
       <header className="header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Header</p>
       </header>
       <main>
         <h1>GuestList</h1>
         <div className="CreateGuest">
           <h2>Add Guest Name:</h2>
-          <div className="FirstName">
-            <label htmlFor="First name">First Name:</label>
-            <input id="First name" />
-          </div>
-          <div className="LastName">
-            <label htmlFor="Last name">First Name:</label>
-            <input id="Last name" />
-          </div>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+            }}
+          >
+            <div className="FirstName">
+              <label htmlFor="First name">First Name:</label>
+              <input
+                id="First name"
+                onChange={(event) => setFirstNameInput(event.target.value)}
+              />
+            </div>
+            <div className="LastName">
+              <label htmlFor="Last name">Last Name:</label>
+              <input
+                id="Last name"
+                onKeyDown={(event) => {
+                  if (event.key === 'Enter') {
+                    const newGuest = {
+                      firstName: firstNameInput,
+                      state: 'not attending',
+                    };
+                    console.log(newGuest);
+                  }
+                }}
+              />
+            </div>
+          </form>
         </div>
+        <hr />
         <div className="GuestList-Section">
           <div className="GuestList">
             <div className="ExampleGuest" key="uniqueID" data-test-id="guest">
@@ -32,6 +54,7 @@ export default function App() {
                 type="checkbox"
                 aria-label="<first name> <last name> <attending status>"
               />
+              <hr />
             </div>
           </div>
         </div>
